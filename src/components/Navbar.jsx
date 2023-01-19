f04fc773340a84dc5e5bfd0dd601093dbd420722
veyseltosun <postalarim1923@gmail.com>
2023-01-19 03:45:15 +0300

    initial...

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    render() {
+
+        <nav>
+            <h2 className="ui centered header">
+                pine academy
+            </h2>
+            <div className="ui three buttons">
+                <button className="ui button">Overview</button>
+                <button className="ui button">Contact</button>
+                <button className="ui button">Support</button>
+            </div>
+        </nav>
+
+    }
+}
