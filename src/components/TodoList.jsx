c2528a9e37b69050b0e8b30589740f48e23221a0
veyseltosun <postalarim1923@gmail.com>
2023-01-20 09:28:23 +0300

    changed to func comp

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,19 @@
-import React from "react";
+import React, {useContext} from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
+const TodoList = () => {
+    const {isDarkTheme, lightTheme, darkTheme, changeTheme} = useContext(ThemeContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    return (
+        <div style={{background:theme.background, color:theme.text, height: "140px", textAlign:"center"}}>
 
-class TodoList extends React.Component {
-    static contextType = ThemeContext;
+        <p className="item">Plane the family trip</p>
+        <p className="item">Go shopping for dinner</p>
+        <p className="item">Go for a walk</p>
+        <button className="ui button primary" onClick={changeTheme}> Change The Theme </button>
 
-    render() {
-        const {isDarkTheme, lightTheme, darkTheme, changeTheme} = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
-
-        return (
-            <div style={{background:theme.background, color:theme.text, height: "140px", textAlign:"center"}}>
-
-                <p className="item">Plane the family trip</p>
-                <p className="item">Go shopping for dinner</p>
-                <p className="item">Go for a walk</p>
-                <button className="ui button primary" onClick={changeTheme}> Change The Theme </button>
-
-            </div>
-        )
-    }
+    </div>
+    )
 }
 
 export default TodoList;
