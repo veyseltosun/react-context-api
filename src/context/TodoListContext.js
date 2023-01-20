0fea5bdc14b5159f9f98fe6f52fc804d2989d91b
veyseltosun <postalarim1923@gmail.com>
2023-01-21 02:20:40 +0300

    ...

--- src/context/TodoListContext.js
+++ src/context/TodoListContext.js
@@ -1,14 +1,19 @@
-import React, { createContext} from "react";
+import React, { createContext } from "react";
 
 export const TodoListContext = createContext();
 
-const TodoListContextProvider = ({children}) => {
-    return(
-        <TodoListContext.Provider>
-            {children}
-        </TodoListContext.Provider>
+const TodoListContextProvider = ({ children }) => {
+    const [todos, setTodos] = useState([
+       {text :"Plane the family trip", id:1},
+       {text: " Go shopping for dinne" , id:2 },     
+       {text :"Go for a walk", id:3},
+    ])
+,return (
+    <TodoListContext.Provider>
+        {children}
+    </TodoListContext.Provider>
 
-    )
+)
 }
 
 export default TodoListContextProvider;
